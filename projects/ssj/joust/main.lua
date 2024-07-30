c0c92f5d9f0a3fafded3f08d963c7738a43d7d79
Cameron Irmas <cirmas@protonmail.com>
2024-07-29 21:20:22 -0700

    Organize

---
+++ projects/ssj/joust/main.lua
@@ -0,0 +1,14 @@
+function _init()
+  -- Set up the game
+end
+
+function _update()
+    -- Update the game
+end
+
+function _draw()
+    cls()
+
+    -- Draw the game
+    circfill(64, 64, 32, 8)
+end
